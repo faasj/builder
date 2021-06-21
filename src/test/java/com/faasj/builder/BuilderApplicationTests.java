61f1d340c86002d0026bc5b76a6b8593ecc03042
Roman Ivanov <roivanoff@gmail.com>
2021-06-21 22:46:20 +0300

    first

---
+++ src/test/java/com/faasj/builder/BuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faasj.builder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuilderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
