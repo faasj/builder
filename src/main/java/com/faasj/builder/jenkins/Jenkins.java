1f6a17dba4d58938d6088b04179f6e7893840b7c
chesnokov-aa <alesha.chesnokov@gmail.com>
2021-07-14 00:23:00 +0300

    update

--- src/main/java/com/faasj/builder/jenkins/Jenkins.java
+++ src/main/java/com/faasj/builder/jenkins/Jenkins.java
@@ -2,29 +2,63 @@ package com.faasj.builder.jenkins;
 import com.offbytwo.jenkins.JenkinsServer;
 import com.offbytwo.jenkins.client.JenkinsHttpClient;
 import com.offbytwo.jenkins.model.Job;
-import com.offbytwo.jenkins.model.JobWithDetails;
 import com.offbytwo.jenkins.model.QueueReference;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
 import java.net.URI;
+import java.net.URISyntaxException;
 
 @Slf4j
+@Component
+@ConfigurationProperties
 public class Jenkins {
 
     private JenkinsServer jenkins;
-    private JenkinsHttpClient jenkinsHttpClient;
 
-    private static final String JENKINS_URL = "http://localhost:8080/";
-    private static final String JENKINS_USER = "admin";
-    private static final String JENKINS_PASSWORD = "11011d22461beb771fdf575d59d77cdf30";
+    @Value("${jenkins.url}")
+    private String url;
 
-    public void init(){
-        try{
-            jenkins = new JenkinsServer(new URI(JENKINS_URL), JENKINS_USER, JENKINS_PASSWORD);
-            jenkinsHttpClient = new JenkinsHttpClient(new URI(JENKINS_URL), JENKINS_USER, JENKINS_PASSWORD);
-        } catch (Exception e){
-            log.error("Failed to Connect to Jenkins Instance");
-            e.printStackTrace();
+    @Value("${jenkins.user}")
+    private String user;
+
+    @Value("${jenkins.password}")
+    private String password;
+
+    long timeToWaitMS = 1000;
+
+    public JenkinsServer getJenkins() {
+
+        int numRetries = 3;
+
+        try {
+            jenkins = new JenkinsServer(new URI(url), user, password);
+        } catch (URISyntaxException e) {
+            log.error("Jenkins URI syntax exception");
+            return null;
         }
+
+        while (!jenkins.isRunning()) {
+            waitUntilNextTry();
+            log.error("retry connection: " + numRetries);
+            numRetries--;
+            if(numRetries <= 0) {
+                log.error("Failed. Jenkins not started.");
+                return null;
+            }
+        }
+        return jenkins;
+    }
+
+    public void waitUntilNextTry()
+    {
+        try {
+            Thread.sleep(timeToWaitMS);
+        }
+        catch (InterruptedException iex) { }
     }
 
     public String build(String jobName) {
@@ -51,7 +85,12 @@ public class Jenkins {
         return "Build job complete";
     }
 
-    public JenkinsServer getJenkins() {
-        return jenkins;
+    public void createFreeStyleJob(String jobName) {
+        try {
+            jenkins.createJob(null, jobName, "freestyle.xml");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        log.info("Created freestyle job "+ jobName);
     }
 }
